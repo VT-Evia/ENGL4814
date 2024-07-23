da950c980f02b2048f4fe9d9345f304d452ed3fa
Carlos Evia <cevia@vt.edu>
2024-07-23 09:20:23 -0400

    Create embedMarkdown.js

---
+++ assets/embedMarkdown.js
@@ -0,0 +1,8 @@
+// assets/js/embedMarkdown.js
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('https://raw.githubusercontent.com/VT-Evia/finalproject/master/_posts/2010-03-07-post-link.md')
+    .then(response => response.text())
+    .then(text => {
+      document.getElementById('markdown-content').innerHTML = marked(text);
+    });
+});
