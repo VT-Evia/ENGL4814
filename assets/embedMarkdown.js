ecaed63d345c36f6a2ec7cd80b4f65e22f7f686f
Carlos Evia <cevia@vt.edu>
2024-07-23 09:52:44 -0400

    Delete assets/embedMarkdown.js

--- assets/embedMarkdown.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('https://raw.githubusercontent.com/VT-Evia/finalproject/master/_posts/2010-03-07-post-link.md')
-    .then(response => response.text())
-    .then(text => {
-      document.getElementById('markdown-content').innerHTML = marked(text);
-    })
-    .catch(error => console.error('Error fetching markdown file:', error));
-});
